47c5c3086546937ea031f50111469a66e553505c
Kulpot <kulpotwahu@gmail.com>
2024-02-21 19:49:51 +0800

    ...

---
+++ C# avatar maker app 13/HeroList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C__heromaker_10;
+
+
+namespace C__avatar_maker_app_12
+{
+    //static class HeroList
+    public class HeroList
+    {
+        public List<Hero> hallOfFame = new List<Hero>();
+    }
+}
